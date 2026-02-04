477850fb66828d4d05ce9f1945e4767948f8da53
Nitin8795 <vishwakarmanitin987@gmail.com>
2026-02-04 17:00:05 +0530

    Initial commit

---
+++ src/main/java/com/my/spring/Main.java
@@ -0,0 +1,29 @@
+package com.my.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.my.spring.model.Employee;
+
+public class Main {
+
+	private static final String Guest = null;
+
+	public static void main(String[] args) {
+		
+		
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
+		
+// 		Employee bean =  (Employee) ioc.getBean("emp");   Type casting Employee
+		
+		Employee bean =  ioc.getBean("emp1",Employee.class);
+		
+		System.out.println(bean);
+		
+//		Employee bean2 =  ioc.getBean("emp2",Employee.class);
+//		
+//		System.out.println(bean2);
+		
+	}
+
+}
